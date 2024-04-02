3f3e022dfcaaba5ed7f76d2b60148dca41a048a2
SambhuBiswakarma00 <biswakarmasambhu@gmail.com>
2024-04-02 20:17:00 +0530

    testing application_start script without any content

--- application_start.sh
+++ application_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Apply Kubernetes deployment and service
-kubectl apply -f /tmp/deployment/kubernetes/deployment.yml
-kubectl apply -f /tmp/deployment/kubernetes/service.yml
+# kubectl apply -f /tmp/deployment/kubernetes/deployment.yml
+# kubectl apply -f /tmp/deployment/kubernetes/service.yml
